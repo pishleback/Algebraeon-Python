d47d665f96c6151b767f6606d3b15192304748ad
Michael <michael@otopia.co.uk>
2026-01-07 19:49:01 +0000

    Update sets.py

--- tests/sets.py
+++ tests/sets.py
@@ -3,6 +3,8 @@ from algebraeon import *
 assert(Nat(3).set() == Nat)
 assert(Int(3).set() == Int)
 assert(Rat(3).set() == Rat)
+assert(Int.polynomials().var().set() == Int.polynomials())
+assert(Rat.polynomials().var().set() == Rat.polynomials())
 
 assert(str(Nat) == "ℕ")
 assert(repr(Nat) == "Nat")
